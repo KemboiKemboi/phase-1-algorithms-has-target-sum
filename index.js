376ddbc8624724ffce6d0259b99613e1a91612d2
Felix Kemboi <felixkemboi824@gmail.com>
2025-03-24 14:09:56 +0300

    initiate commit

--- index.js
+++ index.js
@@ -1,34 +1,17 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
+// Function to check if two numbers in an array sum up to the target
+function hasTargetSum(arr, target) {
+  const seen = new Set();
+
+  for (let num of arr) {
+      const complement = target - num;
+      if (seen.has(complement)) {
+          return true;
+      }
+      seen.add(num);
+  }
+
+  return false;
 }
 
-/* 
-  Write the Big O time complexity of your function here
-*/
+module.exports = hasTargetSum; // Make sure to export the function
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
-
-  console.log("");
-
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
-}
-
-module.exports = hasTargetSum;
